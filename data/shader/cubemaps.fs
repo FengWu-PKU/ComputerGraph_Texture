5046e8d2db6f4bef097eaf09a25a85af2118263f
Feng Wu <2000013727@stu.pku.edu.cn>
2023-05-25 16:54:58 +0800

    modify shaders

--- data/shader/cubemaps.fs
+++ data/shader/cubemaps.fs
@@ -1,11 +1,36 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec2 TexCoords;
+in VS_OUT {
+    vec3 FragPos;
+    vec2 TexCoords;
+    mat3 TBN;
+} fs_in;
 
 uniform sampler2D texture1;
+uniform sampler2D normalMap;
+
+uniform vec3 lightPos;
+uniform vec3 lightColor;
 
 void main()
 {
-    FragColor = texture(texture1, TexCoords);
+    vec3 objectColor = texture(texture1, fs_in.TexCoords).rgb;  // 从纹理图片中获取片段的颜色
+
+    vec3 normal;
+    normal=texture(normalMap, fs_in.TexCoords).rgb;
+    normal = normalize(normal * 2.0 - 1.0);
+    normal = normalize(fs_in.TBN * normal);
+
+    // 环境光照
+    float ambientStrength=0.1;
+    vec3 ambient=ambientStrength*lightColor;
+    // 漫反射
+    vec3 lightDir=normalize(lightPos-fs_in.FragPos);
+    float diff=max(dot(normal, lightDir),0.0);
+    vec3 diffuse=diff*lightColor;
+
+    vec3 result=(ambient+diffuse)*objectColor;
+    FragColor=vec4(result, 1.0f);
+
 }
